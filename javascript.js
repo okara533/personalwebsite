20cdf4bf2e08e15690771f90ac24db3bae5ec46c
okara533 <ozankara@hotmail.com>
2023-05-20 01:43:11 +0300

    button updates and flexbox implemantions

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
 function showText(page){
     document.querySelectorAll(".experiences").forEach(div => div.style.display = "none");
-    document.querySelector(`#${page}`).style.display = "block";
+    if(page=="byTime"){
+        document.querySelector(`#${page}`).style.display = "block";}
+        else{
+            document.querySelector(`#${page}`).style.display = "flex";
+        }
 }
 
 document.addEventListener("DOMContentLoaded", function(){
@@ -8,23 +12,10 @@ document.addEventListener("DOMContentLoaded", function(){
         button.onclick = function(){
             showText(this.dataset.page);
         }
+        
     });
 
     
 
     
-})
-
-    /*
-    #byExperienceButton{
-        box-shadow:inset 0px 1px 0px 0px #3dc21b;
-        background:linear-gradient(to bottom, #44c767 5%, #5cbf2a 100%);
-        background-color:#44c767;
-        border:1px solid #18ab29;
-        text-shadow:0px 1px 0px #2f6627;
-    }
-    
-    #byExperienceButton:hover {
-        background:linear-gradient(to bottom, #5cbf2a 5%, #44c767 100%);
-        background-color:#5cbf2a;
-    }*/
+});
